4e821c982d92339049ce26f36d421214418d5008
Franky Jiang <frankyscoding@gmail.com>
2023-01-19 22:23:19 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,79 @@
+console.log('main.js is loaded');
+import { IMPORTANT_VALUE, logInCaps, Dog } from './import-from-me.js';
+
+function createTriviaHtml(category, question, answer) {
+  return `
+    <div>
+      <h3>${category}</h3>
+      <p><b>Question:</b> ${question}</p>
+      <p><b>Answer:</b> ${answer}</p>
+    </div>
+  `;
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+    console.log('DOM content fully loaded and parsed.');
+
+    const url = "https://jservice.xyz/api/random-clue";
+    try {
+      const response = await fetch (url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        const category = data.category.title;
+        const question = data.question;
+        const answer = data.answer;
+        const html = createTriviaHtml(category, question, answer);
+        const mainTag = document.querySelector('main');
+        mainTag.innerHTML = html;
+      } else {
+        throw new Error('Response not ok');
+      }
+    } catch (error) {
+      console.error('error', error);
+    }
+  });
+
+console.log('IMPORTANT_VALUE is', IMPORTANT_VALUE);
+logInCaps('This is a message.');
+
+const dog = new Dog('Fido');
+const dogWords = dog.speak();
+console.log(dogWords);
+
+console.log('main.js is loaded');
+
+const createClueButton = document.getElementById('create-clue');
+createClueButton.addEventListener('click', async () => {
+  const question = document.getElementById('question').value;
+  const answer = document.getElementById('answer').value;
+  const value = Number.parseInt(document.getElementById('value').value);
+
+  const data = {
+    question: question,
+    answer: answer,
+    value: value,
+    categoryId: 1485,
+  };
+
+  const url = "https://jservice.xyz/api/clues";
+  const fetchOptions = {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  }
+  const response = await fetch(url, fetchOptions);
+  if (response.ok) {
+    const responseData = await response.json();
+
+    const category = responseData.category.title;
+    const question = responseData.question;
+    const answer = responseData.answer;
+    const html = createTriviaHtml(category, question, answer);
+
+    const newClueTag = document.getElementById('new-clue');
+    newClueTag.innerHTML = html;
+  }
+});
